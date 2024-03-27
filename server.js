73b16ee3d4c47a1fd9d3cb1ce83f15cd9cbb4cc3
ComputerCow <mjacobs2@wpi.edu>
2024-03-27 11:13:51 -0400

    beginnings of express

--- server.js
+++ server.js
@@ -1,27 +1,80 @@
+const fs   = require( "fs" ),
+      // IMPORTANT: you must run `npm install` in the directory for this assignment
+      // to install the mime library if you're testing this on your local machine.
+      // However, Glitch will install it automatically by looking in your package.json
+      // file.
+      mime = require( "mime" ),
+      anagram = require("./anagram"),
+      dir  = "public/",
+      port = 3000,
+      express = require("express"),
+      app = express()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.URI;
-// console.log(uri);
+var maxId = 0;
+const appdata = [];
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+const handlePost = function(request, response) {
+    let data = request.body;
+    var type = data.type;
+    switch(type){
+      //Entry is a new anagram request
+      case "anagram":
+        handleNewEntry(response,data);
+        break;
+      //Entry is a request to remove a specific anagram.
+      case "remove":
+        handleRemove(response,data);
+        break;
+      //Entry is a request to get all current appdata.
+      case "getAll":
+        handleGetAll(response,data);
+        break;
+    }
 }
-run().catch(console.dir);
+
+//When a new anagram request comes, sends back the new appdata entry.
+const handleNewEntry = function(response,data){
+  var string = data.string;
+  var anagrams = anagram.getAnagrams(string,4);
+  //Send this back as a unique identifier, which will allow the client to delete entries.
+  let nextData = {
+    id:maxId,
+    string:string,
+    gram0:anagrams[0],
+    gram1:anagrams[1],
+    gram2:anagrams[2],
+    gram3:anagrams[3]
+  };
+  maxId++;
+  appdata.push(nextData);
+  console.log(anagrams);
+
+  response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+  response.end(JSON.stringify(nextData));
+}
+
+//When a request comes in to remove, remove it from appdata and send back the ID the server removed.
+const handleRemove = function(response, data){
+  var removeVal = data.index;
+  for(let i = 0; i < appdata.length; i++){
+    //If the ID isn't found, that's fine. It just won't remove anything.
+    if(appdata[i].id === removeVal){
+      appdata.splice(i,1);
+      break;
+    }
+  }
+  response.writeHead( 200, "OK", {"Content-Type": "text/plain" });
+  response.end(JSON.stringify({index:data.index}));
+}
+
+//Give the client all appdata
+const handleGetAll = function(response,data){
+  response.writeHead( 200, "OK", {"Content-Type": "text/plain" });
+  response.end(JSON.stringify(appdata));
+}
+
+app.use(express.static('public'));
+app.use(express.json());
+app.post('/submit',handlePost);
+console.log(process.env.port);
+app.listen(process.env.PORT);
