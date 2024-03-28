f8cc84042b68b20c2663ecba8ea056d2293957f1
Aidan MacNevin <ahmacnevin@wpi.edu>
2024-03-28 11:14:01 -0400

    Files match a2

---
+++ public/js/main.js
@@ -0,0 +1,200 @@
+// FRONT-END (CLIENT) JAVASCRIPT HERE
+let editOriginalName
+let editOriginalRace
+const submit = async function( event ) {
+  // stop form submission from trying to load
+  // a new .html page for displaying results...
+  // this was the original browser behavior and still
+  // remains to this day
+  event.preventDefault()
+  
+  const charname = document.querySelector( "#charname" ),
+        charrace = document.querySelector( "#charrace" ),
+        charclass = document.querySelector( "#charclass" ),
+        action = document.querySelector('input[name="action"]:checked').value;
+
+    json = { "charname": charname.value,
+        "charrace": charrace.value,
+        "charclass": charclass.value,
+        "action": action},
+        body = JSON.stringify( json )
+
+  const response = await fetch( "/submit", {
+    method:"POST",
+    body 
+  })
+
+  const text = await response.text()
+
+  console.log( "text:", text )
+
+  document.getElementById("characterForm").reset()
+
+  await loadTable()
+}
+
+const deleteEntry = async function (deleteName, deleteRace){
+
+    console.log("delete entry: ", deleteName, deleteRace)
+
+    const json = {
+        "deleteName": deleteName,
+        "deleteRace": deleteRace
+    }
+
+    const body = JSON.stringify(json)
+
+    const response = await fetch( "/delete", {
+        method:"POST",
+        body
+    })
+
+    // const text = await response.text()
+    await loadTable()
+}
+
+const save = async function( event ){
+    event.preventDefault();
+
+    console.log("global var values: ", editOriginalName, editOriginalRace)
+
+    await deleteEntry(editOriginalName, editOriginalRace);
+
+    await saveEdit(event)
+}
+
+
+const editEntry = function (editName, editRace, editClass, editMod, editAction){
+    console.log("edit data for ", editName)
+    document.getElementById("editForm").style.display = 'block';
+    document.getElementById("editname").value = editName;
+    document.getElementById("editrace").value = editRace;
+    document.getElementById("editclass").value = editClass;
+    document.getElementById("editmodifier").value = editMod;
+    document.getElementById("editaction").value = editAction;
+    editOriginalName = editName;
+    editOriginalRace = editRace;
+}
+
+const saveEdit = async function ( event ){
+    event.preventDefault()
+    console.log("saving edit, deleting ", editOriginalName, editOriginalRace)
+
+
+    const charname = document.querySelector( "#editname" ),
+        charrace = document.querySelector( "#editrace" ),
+        charclass = document.querySelector( "#editclass" ),
+        charmod = document.querySelector( "#editmodifier" ),
+        action = document.querySelector('#editaction');
+
+    json = { "name": charname.value,
+        "race": charrace.value,
+        "class": charclass.value,
+        "modifier": charmod.value,
+        "action": action.value},
+        body = JSON.stringify( json )
+
+    const response = await fetch( "/edit", {
+        method:"POST",
+        body
+    })
+
+    cancel(event);
+    await loadTable();
+}
+const cancel = function ( event ){
+    event.preventDefault()
+
+    document.getElementById("editForm").style.display = 'none';
+    document.getElementById("editname").value = "";
+    document.getElementById("editrace").value = "";
+    document.getElementById("editclass").value = "";
+    document.getElementById("editmodifier").value = "";
+    document.getElementById("editaction").value = "";
+
+    editOriginalRace = ""
+    editOriginalName = ""
+
+}
+
+
+const loadTable = async function (){
+    console.log("loading table")
+
+    await fetch('/api/appdata')
+        .then(response => response.json())
+        .then(data => {
+            const existingTableBody = document.getElementById("charTableBody");
+            const tableRows = existingTableBody.getElementsByTagName('tr');
+            const rowCount = tableRows.length;
+            //Remove all non-header rows
+            for (let x=rowCount-1; x>0; x--) {
+                existingTableBody.removeChild(tableRows[x]);
+            }
+            console.log('Appdata from server:', data);
+            for (let i = 0; i < data.length; i++) {
+                // creates a table row
+                const row = document.createElement("tr");
+                for (let j = 0; j < 6; j++) {
+                    // Create a <td> element and a text node, make the text
+                    // node the contents of the <td>, and put the <td> at
+                    // the end of the table row
+                    const cell = document.createElement("td");
+                    let content = "";
+                    switch(j){
+                        case 0:
+                            content = data[i].name
+                            break;
+                        case 1:
+                            content = data[i].race
+                            break;
+                        case 2:
+                            content = data[i].class
+                            break;
+                        case 3:
+                            content = data[i].modifier
+                            break;
+                        case 4:
+                            content = data[i].action
+                            break;
+                        case 5:
+                            const button2 = document.createElement("button")
+                            button2.onclick = () => editEntry(data[i].name, data[i].race, data[i].class, data[i].modifier, data[i].action);
+                            button2.id = "editButton"
+                            button2.textContent = "Edit"
+                            cell.appendChild(button2);
+
+                            const button = document.createElement("button")
+                            button.onclick = () => deleteEntry(data[i].name, data[i].race);
+                            button.id = "deleteButton"
+                            button.textContent = "Delete"
+                            cell.appendChild(button);
+
+
+                            row.appendChild(cell);
+                            break;
+
+                    }
+                    if(j !== 5) {
+                        const cellText = document.createTextNode(content);
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
+                    }
+                }
+
+                // add the row to the end of the table body
+                existingTableBody.appendChild(row);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching appdata:', error);
+        });
+}
+
+window.onload = function() {
+   const submitbutton = document.querySelector("#submitbutton");
+    submitbutton.onclick = submit;
+    document.querySelector("#cancelbutton").onclick = cancel;
+    document.querySelector("#savebutton").onclick = save;
+    loadTable();
+}
---
+++ server.improved.js
@@ -0,0 +1,236 @@
+const http = require( "http" ),
+      fs   = require( "fs" ),
+      // IMPORTANT: you must run `npm install` in the directory for this assignment
+      // to install the mime library if you"re testing this on your local machine.
+      // However, Glitch will install it automatically by looking in your package.json
+      // file.
+      mime = require( "mime" ),
+      dir  = "public/",
+      port = 3000
+
+const appdata = [
+  { "name": "Duolian", "race": "Earth Genasi", "class": "Paladin", "modifier": "Charisma", "action": "Greatsword"},
+  { "name": "Kaede", "race": "Wood Elf", "class": "Monk", "modifier": "Dexterity", "action": "Unarmed Strikes" },
+  { "name": "Thaddeus Thunderclap", "race": "Gnome", "class": "Wizard", "modifier": "Intelligence", "action": "Thunderclap"}
+]
+
+const server = http.createServer( function( request,response ) {
+  if( request.method === "GET" ) {
+    handleGet( request, response )    
+  }else if( request.method === "POST" ){
+    handlePost( request, response ) 
+  }
+})
+
+const handleGet = function( request, response ) {
+  const filename = dir + request.url.slice( 1 ) 
+
+  if( request.url === "/" ) {
+    sendFile( response, "public/index.html" )
+  }else if (request.url === "/api/appdata") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(appdata));
+  }
+  else{
+    sendFile( response, filename )
+  }
+}
+
+const handlePost = function( request, response ) {
+  let dataString = ""
+
+  request.on( "data", function( data ) {
+      dataString += data 
+  })
+
+  request.on( "end", function() {
+    // console.log( JSON.parse( dataString ) )
+
+    if (request.url === "/submit") {
+      //handle submit code
+      saveData(dataString);
+    } else if(request.url === "/edit") {
+      saveEdit(dataString);
+    }
+    else if(request.url === "/delete"){
+      console.log("received delete request")
+      //handle delete code
+      deleteData(dataString)
+    }
+
+    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
+    response.end("Character Information Received")
+  })
+}
+
+const saveData = function( jsonData ){
+  let myDataJSON = JSON.parse( jsonData )
+
+  let charModifier = "unknown"
+  let randAction = "Unarmed Strikes"
+
+  console.log("class :", myDataJSON.charclass)
+
+  const charClass = myDataJSON.charclass.toString().toLowerCase();
+
+  //Either weapon or spell
+  const actionType = myDataJSON.action;
+
+
+  if (charClass === "artificer" || charClass === "wizard") {
+    charModifier = "Intelligence";
+  } else if (charClass === "druid" || charClass === "ranger" || charClass === "cleric") {
+    charModifier = "Wisdom";
+  } else if (charClass === "paladin" || charClass === "bard" || charClass === "sorcerer" || charClass === "warlock") {
+    charModifier = "Charisma";
+  } else if (charClass === "monk" || charClass === "rogue") {
+    charModifier = "Dexterity";
+  } else if (charClass === "barbarian" || charClass === "fighter") {
+    charModifier = "Strength";
+  } else {
+    charModifier = "unknown";
+  }
+
+  if (actionType === "weapon") {
+    switch (charClass) {
+      case "artificer":
+        randAction = "gun";
+        break;
+      case "wizard":
+        randAction = "Quarterstaff";
+        break;
+      case "druid":
+        randAction = "Quarterstaff";
+        break;
+      case "ranger":
+        randAction = "Longbow";
+        break;
+      case "cleric":
+        randAction = "Mace";
+        break;
+      case "paladin":
+        randAction = "Warhammer";
+        break;
+      case "bard":
+        randAction = "Lute";
+        break;
+      case "sorcerer":
+        randAction = "Quarterstaff";
+        break;
+      case "warlock":
+        randAction = "Hexblade";
+        break;
+      case "monk":
+        randAction = "Unarmed Strikes";
+        break;
+      case "rogue":
+        randAction = "Dagger";
+        break;
+      case "barbarian":
+        randAction = "Greataxe";
+        break;
+      case "fighter":
+        randAction = "Longsword";
+        break;
+      default:
+        // Default case
+        break;
+    }
+  } else { // actionType === "magic"
+    switch (charClass) {
+      case "artificer":
+        randAction = "Arcane Weapon";
+        break;
+      case "wizard":
+        randAction = "Fireball";
+        break;
+      case "druid":
+        randAction = "Entangle";
+        break;
+      case "ranger":
+        randAction = "Hunter's Mark";
+        break;
+      case "cleric":
+        randAction = "Cure Wounds";
+        break;
+      case "paladin":
+        randAction = "Divine Smite";
+        break;
+      case "bard":
+        randAction = "Vicious Mockery";
+        break;
+      case "sorcerer":
+        randAction = "Magic Missile";
+        break;
+      case "warlock":
+        randAction = "Eldritch Blast";
+        break;
+      case "monk":
+        randAction = "Slow Fall";
+        break;
+      case "rogue":
+        randAction = "Invisibility";
+        break;
+      case "barbarian":
+        randAction = "RAGE!!";
+        break;
+      case "fighter":
+        randAction = "Action Surge";
+        break;
+      default:
+        // Default case
+        randAction = "Firebolt";
+        break;
+    }
+  }
+
+  appdata.push({"name": myDataJSON.charname, "race": myDataJSON.charrace, "class": myDataJSON.charclass, "modifier": charModifier, "action": randAction})
+
+  console.log(appdata)
+}
+
+const saveEdit = function( jsonData ){
+  let myDataJSON = JSON.parse( jsonData )
+  appdata.push({"name": myDataJSON.name, "race": myDataJSON.race, "class": myDataJSON.class, "modifier": myDataJSON.modifier, "action": myDataJSON.action})
+}
+const deleteData = function( jsonData ){
+  console.log("deleting based on jsondata ", jsonData )
+  let myDataJSON = JSON.parse( jsonData )
+  const deleteName = myDataJSON.deleteName;
+  const deleteRace = myDataJSON.deleteRace;
+
+  for (let i = 0; i < appdata.length; i++) {
+    // Check if the current object's name and race match the provided values
+    if (appdata[i].name === deleteName && appdata[i].race === deleteRace) {
+      // Remove the object from the array
+      appdata.splice(i, 1);
+      // Break out of the loop since we've found and removed the entry
+      break;
+    }
+  }
+
+}
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we"ve loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { "Content-Type": type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( "404 Error: File Not Found" )
+
+     }
+   })
+}
+
+server.listen( process.env.PORT || port )
