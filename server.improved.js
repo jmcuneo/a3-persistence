762d33a370ad42f3683109017bc79f39078fea1c
hanzalah416 <hanzalah.qamar02@gmail.com>
2024-03-28 02:33:02 -0400

    callback git auth

--- server.improved.js
+++ server.improved.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/github/callback"
+        callbackURL: "https://a3-persistence.vercel.app/auth/github/callback"
     },
     async function(accessToken, refreshToken, profile, done) {
         try {
