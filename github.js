753b9219ed416db2a55b2973324827a887c4ff2b
ibixler <108148378+ibixler@users.noreply.github.com>
2024-03-29 11:53:08 -0400

    teehee it works i did some ungodly things to get here
    it doesnt actually work I can just get to github and hit authorize which is a start horray!!!!!

--- github.js
+++ github.js
@@ -3,23 +3,56 @@ const app = express()
 var GitHubStrategy = require('passport-github2').Strategy
 const passport = require('passport')
 const env = require('dotenv').config()
+var session = require('express-session')
+const path = require('path'); 
+
+var util = require('util');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var partials = require('express-partials');
+
+app.set('trust proxy', 0) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+app.use(partials());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/github/callback"
+    callbackURL: "http://localhost:3001/auth/callback/github"
     },
-    function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-        return done(err, user);
-    });
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ githubId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
     }
 ));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+/* app.use(passport.session()); */
 
 app.use(express.static('public'))
 app.use(express.static('views'))
 
-app.get("/auth", passport.authenticate('passport-github2'));
-
-
-app.listen(process.env.PORT || 3000)
+app.get("/auth", passport.authenticate('github', {scope: ['user:email']}),
+    function(req,res){
+        console.log('req made')
+    });
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+app.get('/auth/callback/github', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+app.listen(process.env.PORT || 3001)
 
