fb29f7edfa560e9ca3b3049a72e732b02248f047
BryTom <BryonTom13@gmail.com>
2024-03-30 23:22:53 -0400

    Express works

--- server.improved.js
+++ server.improved.js
@@ -1,49 +1,53 @@
-const express    = require('express'),
-    app        = express(),
-    entries     = [{
-      name: "Bryon Tom",
-      bodyWeight: 132,
-      squat: 297,
-      benchPress: 171,
-      deadLift: 357,
-      total: 825
-    }]
+const express = require('express');
+const { MongoClient, ObjectId } = require("mongodb")
+const app = express();
+const port = 3000;
+const entries = [{
+    name: "Bryon Tom",
+    bodyWeight: 132,
+    squat: 297,
+    benchPress: 171,
+    deadLift: 357,
+    total: 825
+}];
 
-app.use( express.static( 'public' ) )
-app.use( express.static( 'views'  ) )
+app.use(express.static('public'));
+app.use(express.static('views'));
 
 const middleware_post = (req, res, next) => {
-  let dataString = ''
+    let dataString = '';
 
-  req.on( 'data', function( data ) {
-    dataString += data
-  })
+    req.on('data', function (data) {
+        dataString += data;
+    });
 
-  req.on( 'end', function() {
-    const json = JSON.parse( dataString )
-    json.total = Number(json.squat) + Number(json.benchPress) + Number(json.deadLift);
+    req.on('end', function () {
+        const json = JSON.parse(dataString);
+        json.total = Number(json.squat) + Number(json.benchPress) + Number(json.deadLift);
 
-    entries.push( json )
+        entries.push(json);
 
-    // add a 'json' field to our request object
-    // this field will be available in any additional
-    // routes or middleware.
-    req.body = JSON.stringify( entries )
+        // add a 'json' field to our request object
+        // this field will be available in any additional
+        // routes or middleware.
+        req.json = JSON.stringify(entries);
 
-    // advance to next middleware or route
-    next()
-  })
-}
+        // advance to next middleware or route
+        next();
+    });
+};
 
-app.post( '/submit', middleware_post(), (req, res) => {
-  //entries.push( req.body )
-  res.writeHead( 200, { 'Content-Type': 'text/plain' })
-  res.end( JSON.stringify( entries ) )
-})
+app.post('/submit', middleware_post, (req, res) => {
+    //entries.push( req.body )
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(JSON.stringify(entries));
+});
 
 app.get('/entries', (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(appdata));
-})
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(entries));
+});
 
-app.listen( process.env.PORT )
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
