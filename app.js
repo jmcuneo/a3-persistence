cdd9175c5ca5da8cc408e6abdad2fbb445728798
spengre <spencerscottg@gmail.com>
2024-03-31 17:09:35 -0400

    static js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.engine('handlebars', handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 app.use(express.json());
+app.use(express.static("public"))
 app.use(session({
 	secret: 'sessionSecretHehe',
 	resave: false,
