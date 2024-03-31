083ba6bcce61daaaf71e639c38c46552f53e2232
Austin Rebello <55983501+AustinRebello@users.noreply.github.com>
2024-03-31 00:41:21 -0400

    70% Assignment Completion
    Made enough progress to secure a grade of 70% if it was submitted now.
    
    Added the functionality of only showing book's that the current signed in user has read.
    Added bootstrap and started to add design to the site using the added framework.
    Removed all old CSS to accommodate bootstrap.
    Changed the forms and data structure from generic people information to the books per user account.
    Added the mongoDB connection to pull from the external database.
    Added a nav bar with a logo (WPI).

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,20 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
+
+function getGenre(formType) {
+  var genres = document.getElementsByName(formType);
+ 
+  for (i = 0; i < genres.length; i++) {
+    if (genres[i].checked)
+      return genres[i].value
+    }
+
+  return null
+}
+
+
 // Function that will return the list of people stored in the server
-var getPeople = async function () {
+var getBooks = async function () {
 
   // Makes the get request to get the array of people from the server
   var response = await fetch ( "/getPeople", {
@@ -10,10 +23,10 @@ var getPeople = async function () {
 
   // Parses the returned data into a readable format
   var text = await response.text();
-  let peopleData = JSON.parse(text);
+  let books = JSON.parse(text);
 
   // Returns to the function caller
-  return peopleData
+  return books
 }
 
 // Function that will submit a post request adding a person to the list in the server
@@ -25,8 +38,9 @@ const submitAdd = async function( event ) {
   event.preventDefault();
 
   // Takes the first and last name of the person as well as their age
-  const input = document.querySelectorAll( "#firstName, #lastName, #age" ),
-        json = { firstName: input[0].value, lastName: input[1].value, age: input[2].value},
+  const input = document.querySelectorAll( "#title, #author, #pagenum, #dateread" ),
+        genreSelect = getGenre('genre')
+        json = { bookTitle: input[0].value, author: input[1].value, genre: genreSelect, pageCount: input[2].value, dateCompleted: input[3].value },
         body = JSON.stringify( json );
 
   // Makes the post request with the body data
@@ -38,7 +52,7 @@ const submitAdd = async function( event ) {
   // Resets the forms to clear the values and then refreshes the table data to reflect changes
   const text = await response.text();
   document.getElementById("formOne").reset();
-  addDataToTable(peopleTable);
+  addDataToTable();
 
 };
 
@@ -64,7 +78,7 @@ const submitRemove = async function( event ) {
   // Resets the forms to clear the values and then refreshes the table data to reflect changes
   const text = await response.text();
   document.getElementById("formTwo").reset();
-  addDataToTable(peopleTable);
+  addDataToTable();
 };
 
 // Function that will submit a post request modifying a person from the list in the server
@@ -76,8 +90,9 @@ const submitEdit = async function( event ) {
   event.preventDefault();
 
   // Takes the first and last name of the person as well as their age
-  const input = document.querySelectorAll( "#idEdit, #firstNameEdit, #lastNameEdit, #ageEdit" ),
-        json = { id: input[0].value, firstName: input[1].value, lastName: input[2].value, age: input[3].value},
+  const input = document.querySelectorAll( "#idEdit, #titleEdit, #authorEdit, #pagenumEdit, #datereadEdit" ),
+        genreSelect = getGenre('genreEdit')
+        json = { id: input[0].value, bookTitle: input[1].value, author: input[2].value, genre: genreSelect, pageCount: input[3].value, dateCompleted: input[4].value },
         body = JSON.stringify( json );
 
   // Makes the post request with the body data
@@ -89,7 +104,8 @@ const submitEdit = async function( event ) {
   // Resets the forms to clear the values and then refreshes the table data to reflect changes
   const text = await response.text();
   document.getElementById("formThree").reset();
-  addDataToTable(peopleTable);
+
+  addDataToTable();
 
 };
 
@@ -111,28 +127,29 @@ window.onload = function() {
     buttonEdit.onclick = submitEdit;
 
     // Retireves the people table itself for building the table with the list from the server
-    var peopleTable = document.querySelector("#peopleTable");
-    addDataToTable(peopleTable);
+
+    addDataToTable();
 };
 
 // Function that retrieves the people data from the server and uses for loops
 // to build an HTML table's rows out with said data
 async function addDataToTable(table){
-
+  table = document.querySelector("#booksTable");
   table.innerHTML = "";
 
-  let peopleData = await getPeople();
-  let columnNames = Object.keys(peopleData[0])
+  let booksRead = await getBooks();
+  let columnNames = ["ID", "Book Title", "Author", "Genre", "Page Count", "Date Completed", "Username"]
   let tableHead = table.createTHead();
   let row = tableHead.insertRow();
 
   // For each person in the list, it builds out a row with that entry
-  for (let person of peopleData){
+  for (let book of booksRead){
     let row = table.insertRow();
+    row.setAttribute("scope", "row")
 
-    for (const property in person) {
+    for (const property in book) {
       let data = row.insertCell();
-      let content = document.createTextNode(person[property]);
+      let content = document.createTextNode(book[property]);
       data.appendChild(content);
     }
   }
@@ -141,6 +158,7 @@ async function addDataToTable(table){
   for (let name of columnNames){
     let th = document.createElement("th");
     let cName = document.createTextNode(name);
+    th.setAttribute("scope","col")
     th.appendChild(cName);
     row.appendChild(th);
   }
--- server.improved.js
+++ server.improved.js
@@ -1,17 +1,26 @@
 const express = require( 'express' ),
       cookie  = require( 'cookie-session' ),
+      { MongoClient, ObjectId } = require("mongodb"),
       hbs     = require( 'express-handlebars' ).engine,
       app     = express()
 
+// FOR GLITCH OR OTHER SERVER: const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
+// FOR TESTING
+const uri = `mongodb+srv://austinwebwareuser:austinwebwareuser@webwareclustera3.utwkatb.mongodb.net/`
 
-var people = [
-  { "id": 1, "firstName": "Mark", "lastName": "Stevenson", "age": 23, "fullName": "Mark Stevenson" },
-  { "id": 2, "firstName": "Tom", "lastName": "Sanford", "age": 30, "fullName": "Tom Sanford" },
-  { "id": 3, "firstName": "Steve", "lastName": "Smith", "age": 14, "fullName": "Steve Smith" } 
-]
+const client = new MongoClient( uri )
 
-var currentID = 4
+let collectionBooks = null
+let collectionLogin = null
+let currentUserLoggedIn = null
 
+async function run() {
+  await client.connect()
+  collectionBooks = await client.db("webware").collection("books_read")
+  collectionLogin = await client.db("webware").collection("users")
+}
+
+run()
 
 // use express.urlencoded to get data sent by defaut form actions
 // or GET requests
@@ -34,32 +43,41 @@ app.use( cookie({
   keys: ['key1', 'key2']
 }))
 
-app.post( '/login', (req,res)=> {
+app.post( '/login', async (req,res)=> {
   debugger
   // express.urlencoded will put your key value pairs 
   // into an object, where the key is the name of each
   // form field and the value is whatever the user entered
-  console.log( req.body )
   
-  // below is *just a simple authentication example* 
-  // for A3, you should check username / password combos in your database
-  if( req.body.password === 'test' ) {
-    // define a variable that we can check in other middleware
-    // the session object is added to our requests by the cookie-session middleware
+  const user = { username: req.body.username}
+  const dbResult = await collectionLogin.findOne(user)
+
+  if( dbResult === null) {
+
+    collectionLogin.insertOne(req.body)
     req.session.login = true
-    
-    // since login was successful, send the user to the main content
-    // use redirect to avoid authentication problems when refreshing
-    // the page or using the back button, for details see:
-    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    currentUserLoggedIn = req.body.username
     res.redirect( 'main.html' )
-  }else{
-    // cancel session login in case it was previously set to true
-    req.session.login = false
-    // password incorrect, send back to login page
-    res.render('index', { msg:'login failed, please try again', layout:false })
+
+  } else {
+
+    if(dbResult.password === req.body.password){
+
+      req.session.login = true
+      currentUserLoggedIn = req.body.username
+      res.redirect( 'main.html' )
+
+    } else {
+
+      req.session.login = false
+      res.render('index', { msg:'Your login failed! Please try again with a new username or a valid password.', layout:false })
+
+    }
   }
 })
+
+
+
 
 app.get( '/', (req,res) => {
   res.render( 'index', { msg:'', layout:false })
@@ -73,12 +91,32 @@ app.use( function( req,res,next) {
     res.render('index', { msg:'login failed, please try again', layout:false })
 })
 
-app.get( '/main.html', ( req, res) => {
-    res.render( 'main', { msg:'success you have logged in', layout:false })
+// middleware that checks if the database connection was successful
+app.use( (req,res,next) => {
+  if( collectionBooks !== null ) {
+    next()
+  }else{
+    res.status( 503 ).send()
+  }
 })
 
-app.get( '/getPeople', (req, res) => {
-  res.end(JSON.stringify(people));
+app.get( '/index.html', ( req, res) => {
+
+  res.render( 'index', { msg:'', layout:false })
+
+})
+
+app.get( '/main.html', ( req, res) => {
+
+  res.render( 'main', { msg:'Success! You have logged in '+currentUserLoggedIn+"!", layout:false })
+
+})
+
+app.get( '/getPeople', async (req, res) => {
+
+  const docs = await collectionBooks.find({username: currentUserLoggedIn}).toArray()
+  res.end( JSON.stringify(docs) )
+
 })
 
 
@@ -90,16 +128,14 @@ app.post( '/submitAdd', (req, res) => {
       dataString += data 
   })
 
-  req.on( "end", function() {
+  req.on( "end", async function() {
     // ... do something with the data here!!!
     let url = req.url;
     let data = JSON.parse( dataString )
 
-    console.log(dataString)
+    data.username = currentUserLoggedIn
 
-    let fullName = data.firstName+" "+data.lastName
-    people.push({id: currentID, firstName: data.firstName, lastName: data.lastName, age: data.age, fullName: fullName})
-    currentID = currentID + 1
+    const result = await collectionBooks.insertOne( data)
 
     res.writeHead( 200, "OK", {"Content-Type": "text/plain" })
     res.end()
@@ -114,19 +150,14 @@ app.post( '/submitRemove', (req, res) => {
       dataString += data 
   })
 
-  req.on( "end", function() {
+  req.on( "end", async function() {
     // ... do something with the data here!!!
     let url = req.url;
     let data = JSON.parse( dataString )
 
-    console.log(dataString)
-
-    for (let index = 0; index < people.length; index++){
-      if (people[index].id == data.id){
-        people.splice(index,1)
-        break
-      }
-    }
+    const result = await collectionBooks.deleteOne({ 
+      _id: new ObjectId( data.id ) 
+    })
 
     res.writeHead( 200, "OK", {"Content-Type": "text/plain" })
     res.end()
@@ -141,128 +172,29 @@ app.post( '/submitEdit', (req, res) => {
       dataString += data 
   })
 
-  req.on( "end", function() {
+  req.on( "end", async function() {
     // ... do something with the data here!!!
     let url = req.url;
     let data = JSON.parse( dataString )
+    let nonEmptyFields = {}
 
-    console.log(dataString)
-
-    for (let index = 0; index < people.length; index++){
-      if (people[index].id == data.id){
-        people[index].firstName = data.firstName;
-        people[index].lastName = data.lastName;
-        people[index].age = data.age;
-        people[index].fullName = data.firstName + " " + data.lastName;
-        break
+    for (field in data){
+      if(!(data[field] == '' || data[field] == null || field == "id")){
+        nonEmptyFields[field] = data[field] 
       }
     }
+
+    console.log(nonEmptyFields)
+
+    const result = await collectionBooks.updateOne(
+      { _id: new ObjectId( data.id ) },
+      { $set:nonEmptyFields }
+    )
 
     res.writeHead( 200, "OK", {"Content-Type": "text/plain" })
     res.end()
   })
 })
 
-
 app.listen( process.env.PORT || 3000 )
 
-/**
-const http = require( "http" ),
-      fs   = require( "fs" ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you"re testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( "mime" ),
-      dir  = "public/",
-      port = 3000
-
-var currentID = 4
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === "GET" ) {
-    handleGet( request, response )    
-  }else if( request.method === "POST" ){
-    handlePost( request, response ) 
-  }
-})
-
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === "/" ) {
-    sendFile( response, "public/index.html" )
-  } else if (request.url === "/getPeople") {
-    response.end(JSON.stringify(people));
-  }else{
-    sendFile( response, filename )
-  }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ""
-
-  request.on( "data", function( data ) {
-      dataString += data 
-  })
-
-  request.on( "end", function() {
-    // ... do something with the data here!!!
-    let url = request.url;
-    let data = JSON.parse( dataString )
-
-    if(url == "/submitAdd"){
-      // Pushes a new person to the people array using the parsed data
-      let fullName = data.firstName+" "+data.lastName
-      people.push({id: currentID, firstName: data.firstName, lastName: data.lastName, age: data.age, fullName: fullName})
-      currentID = currentID + 1
-    } else if (url == "/submitRemove") {
-      // Searches through the people list until it finds the matching ID
-      for (let index = 0; index < people.length; index++){
-        if (people[index].id == data.id){
-          people.splice(index,1)
-          break
-        }
-      }
-    } else if (url == "/submitEdit"){
-      // Searches through the people list until it finds the matching ID
-      for (let index = 0; index < people.length; index++){
-        if (people[index].id == data.id){
-          people[index].firstName = data.firstName;
-          people[index].lastName = data.lastName;
-          people[index].age = data.age;
-          people[index].fullName = data.firstName + " " + data.lastName;
-          break
-        }
-      }
-    }
-
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end()
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we"ve loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { "Content-Type": type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( "404 Error: File Not Found" )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
- */
