cca5da6e6164ba5fcfceb1e1ae4d470dc7f812c7
spengre <spencerscottg@gmail.com>
2024-03-30 22:33:06 -0400

    testing, no auth

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "https://a3-persistence.vercel.app/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
@@ -65,17 +65,17 @@ const ensureAuthenticated = function (req, res, next) {
 
 
 
-app.get('/auth/github',
-	passport.authenticate('github', { scope: ['user:email'] }));
+// app.get('/auth/github',
+// 	passport.authenticate('github', { scope: ['user:email'] }));
 
-app.get('/auth/github/callback',
-	passport.authenticate('github', { failureRedirect: '/login' }),
-	function (req, res) {
-		// Successful authentication, redirect home.
-		res.redirect('/');
-	});
+// app.get('/auth/github/callback',
+// 	passport.authenticate('github', { failureRedirect: '/login' }),
+// 	function (req, res) {
+// 		// Successful authentication, redirect home.
+// 		res.redirect('/');
+// 	});
 
-app.get("/", ensureAuthenticated, (req, res) => {
+app.get("/", (req, res) => {
 	// shifts = [
 	// 	{id: "1", date: "2", start: "3", end: "4", duration: "5" },
 	// 	{id: "10", date: "9", start: "8", end: "7", duration: "6" },
