c96c3d04d46d765dfcfb0c40a4bf355e4ca8dbe9
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2024-04-01 11:01:37 -0400

    setup (carryover from a2)

---
+++ public/README.txt
@@ -0,0 +1,24 @@
+Design Achievements for A2:
+
+
+Single-page App:
+
+Entering the appropriate information in all three fields on the webpage will create a new filed derived from
+Score / time, which is calculated on the server and shown immediately on the client.
+
+
+Modifying Existing Data:
+
+Entering a name that already exists on the table in the username filed
+will update their existing information, except for the date (which was an intentional choice)
+
+
+Web Testing with other students:
+
+Brandon Lui: Commented on the fact that I had pre-entered values and could change them to be in the background of the text that way
+when you click on the input fields they are empty but still tell you what they are for
+
+Jayson Caissie: Found a bug that when editing information that the Score / Time filed did not round ot one decimal place (this has been fixed)
+
+
+
---
+++ public/js/main.js
@@ -0,0 +1,104 @@
+// FRONT-END (CLIENT) JAVASCRIPT HERE
+
+let tableIndex = 0;
+
+const submit = async function( event ) {
+  // stop form submission from trying to load
+  // a new .html page for displaying results...
+  // this was the original browser behavior and still
+  // remains to this day
+  event.preventDefault()
+  
+  const input = document.querySelector( "#userinput"),
+        json = { username: input[0].value, score: input[1].value, time: input[2].value },
+        body = JSON.stringify( json )
+
+
+  const response = await fetch( "/submit", {
+    method:"POST",
+    body 
+  })
+
+  const text = await response.text()
+
+
+  console.log( "text:", text )
+
+  let newData = { "username": input[0].value, "score": input[1].value, "time": input[2].value }
+
+  location.reload()
+
+}
+
+window.onload = async function() {
+  const button = document.querySelector("button");
+  button.onclick = submit;
+
+
+  //WORKS!
+  const populateTable = await fetch("/appdata",
+      {
+        method:"GET"
+      })
+
+  let data = await populateTable.json();
+
+ // console.log(data)
+
+  for(let i = 0; i < data.length; i++)
+  {
+    if(data[i] !== undefined)
+    {
+      insetElementToTable(data[i])
+    }
+
+  }
+
+}
+
+
+//CUSTOM CODE: Add this to the table
+async function insetElementToTable(element)
+{
+  tableIndex++;
+  const table = document.getElementById("InformationTable")
+  let row = table.insertRow(tableIndex)
+
+  for(let i = 0; i <= 5; i++)
+  {
+    let cell = row.insertCell(i)
+
+    if(i == 0)
+    {
+      cell.innerHTML = element.username;
+    }
+    else if(i == 1)
+    {
+      cell.innerHTML = element.score;
+    }
+    else if(i == 2)
+    {
+      cell.innerHTML = element.time;
+    }
+    else if(i == 3)
+    {
+      cell.innerHTML = element.scoreOverTime;
+      //cell.innerHTML =  Math.round((element.score / element.time) * 10) / 10;
+    }
+    else if(i == 4)
+    {
+      cell.innerHTML = element.date;
+      //let curDate = new Date()
+      //cell.innerHTML = (curDate.getMonth() + 1) + "/" + curDate.getDate() + "/" + curDate.getFullYear()
+    }
+    else if(i == 5)
+    {
+      //cell.innerHTML = tableIndex;
+      cell.innerHTML = element.ID;
+    }
+
+  }
+
+
+
+}
