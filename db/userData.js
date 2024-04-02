8536a0edd4c83a61e95fa0f45b07aaaaab7cf486
spengre <spencerscottg@gmail.com>
2024-04-02 09:22:34 -0400

    fix usr

--- db/userData.js
+++ db/userData.js
@@ -6,16 +6,27 @@ const getNextID = async function(username) {
 	const userQuery = { user: username }
 	let nextID = -1;
 
-	const user = await userColl.findOne(userQuery);
-	if (user) {
-		nextID = (user.idLast) + 1;
-		const original = {_id: user._id};
-		const newVals = { $set: { idLast: nextID }};
-		const result = await userColl.updateOne(original, newVals);
-	} else {
-		await userColl.insertOne({user: username, idLast: 0})
-		nextID = 0;
-	}
+	const user = await userColl.findOne(userQuery).then(async (doc) => {
+		if (!doc) {
+			await userColl.insertOne({user: username, idLast: 0})
+			nextID = 0;
+		} else {
+			nextID = (user.idLast) + 1;
+			const original = {_id: user._id};
+			const newVals = { $set: { idLast: nextID }};
+		 	await userColl.updateOne(original, newVals);
+		}
+	});
+	
+	// if (user) {
+	// 	nextID = (user.idLast) + 1;
+	// 	const original = {_id: user._id};
+	// 	const newVals = { $set: { idLast: nextID }};
+	// 	const result = await userColl.updateOne(original, newVals);
+	// } else {
+	// 	await userColl.insertOne({user: username, idLast: 0})
+	// 	nextID = 0;
+	// }
 	
 	return nextID;
 }
