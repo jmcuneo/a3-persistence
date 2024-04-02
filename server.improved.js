b934c0430a9d403126024457af6a1fc26ba97c41
Seth Frank <sbfrank@wpi.edu>
2024-04-01 21:26:43 -0400

    update vercel.json

--- server.improved.js
+++ server.improved.js
@@ -122,7 +122,8 @@ app.post( '/login', async (request,response)=> {
 
 
 app.get( '/', (req,res) => {
-  res.render( 'login', { msg:'', layout:false })
+  //res.render( 'login', { msg:'', layout:false })
+  res.send("Express on Vercel")
 })
 
 app.get( '/login.html', (req,res) => {
@@ -263,6 +264,7 @@ function determinePriority(data) {
 }
 
 const listener = app.listen( process.env.PORT || 3000 )
+module.exports = app;
 
 
 
