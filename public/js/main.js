cb4c9682ffd2e519a218bc3e59dbf44c44b066a7
Gabriel Shiu <gabriel@shiu.us>
2024-04-03 18:36:15 -0400

    prettier

--- public/js/main.js
+++ public/js/main.js
@@ -1,68 +1,67 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
-let tasks = []
-let editIndex = -1
+let tasks = [];
+let editIndex = -1;
 
-const renderTasks = function() {
-  const table = document.querySelector("#tasks")
-  table.innerHTML = ""
-  const headerRow = document.createElement("tr")
-  headerRow.innerHTML = `<th>Done</th><th>Task Name</th><th>Priority</th><th>Creation Date</th><th>Days Not Done</th>`
-  table.appendChild(headerRow)
+const renderTasks = function () {
+  const table = document.querySelector("#tasks");
+  table.innerHTML = "";
+  const headerRow = document.createElement("tr");
+  headerRow.innerHTML = `<th>Done</th><th>Task Name</th><th>Priority</th><th>Creation Date</th><th>Days Not Done</th>`;
+  table.appendChild(headerRow);
 
   for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i]
-    const row = document.createElement("tr")
+    const task = tasks[i];
+    const row = document.createElement("tr");
     if (i === editIndex) {
-      const editForm = document.createElement("form")
-      editForm.id = "editForm"
+      const editForm = document.createElement("form");
+      editForm.id = "editForm";
       editForm.onsubmit = async function (event) {
-        event.preventDefault()
-        const form = document.querySelector( "#editForm" )
-        const formData = new FormData(form)
-        const json = {}
-        formData.forEach(function(value, key){
+        event.preventDefault();
+        const form = document.querySelector("#editForm");
+        const formData = new FormData(form);
+        const json = {};
+        formData.forEach(function (value, key) {
           json[key] = value;
-        })
-        json["_id"] = tasks[i]._id
-        const body = JSON.stringify( json )
-        const response = await fetch( "/tasks", {
-          method:"PUT",
+        });
+        json["_id"] = tasks[i]._id;
+        const body = JSON.stringify(json);
+        const response = await fetch("/tasks", {
+          method: "PUT",
           headers: {
             "Content-Type": "application/json",
           },
           body: body,
-        })
+        });
 
-        editIndex = -1
+        editIndex = -1;
 
         // console.log( response.json() )
-        await updateTasks()
-      }
+        await updateTasks();
+      };
       // table.appendChild(editForm)
-      row.innerHTML = `<input type="checkbox" class="trollCheckbox"/><td><input name="taskName" type="text" form="editForm" value="${task.taskName}" required></td><td><input name="priority" type="number" form="editForm" value="${task.priority}" required></td><td><input name="creation_date" type="date" form="editForm" value="${task.creation_date}" required></td><td></td><input id="edit" type="submit" form="editForm" value="Submit"/>`
-      row.appendChild(editForm)
+      row.innerHTML = `<input type="checkbox" class="trollCheckbox"/><td><input name="taskName" type="text" form="editForm" value="${task.taskName}" required></td><td><input name="priority" type="number" form="editForm" value="${task.priority}" required></td><td><input name="creation_date" type="date" form="editForm" value="${task.creation_date}" required></td><td></td><input id="edit" type="submit" form="editForm" value="Submit"/>`;
+      row.appendChild(editForm);
 
-      const cancelButton = document.createElement("button")
-      cancelButton.innerHTML = "Cancel"
+      const cancelButton = document.createElement("button");
+      cancelButton.innerHTML = "Cancel";
       cancelButton.onclick = function () {
-        editIndex = -1
-        renderTasks()
-      }
-      row.appendChild(cancelButton)
-    }
-    else {
-      row.innerHTML = `<input type="checkbox" class="trollCheckbox"/><td>${task.taskName}</td><td>${task.priority}</td><td>${task.creation_date}</td><td>${task.days_not_done}</td>`
+        editIndex = -1;
+        renderTasks();
+      };
+      row.appendChild(cancelButton);
+    } else {
+      row.innerHTML = `<input type="checkbox" class="trollCheckbox"/><td>${task.taskName}</td><td>${task.priority}</td><td>${task.creation_date}</td><td>${task.days_not_done}</td>`;
 
-      const editButton = document.createElement("button")
-      editButton.innerHTML = "Edit"
+      const editButton = document.createElement("button");
+      editButton.innerHTML = "Edit";
       editButton.onclick = function () {
-        editIndex = i
-        renderTasks()
-      }
-      row.appendChild(editButton)
+        editIndex = i;
+        renderTasks();
+      };
+      row.appendChild(editButton);
 
-      const deleteButton = document.createElement("button")
-      deleteButton.innerHTML = "Delete"
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "Delete";
       deleteButton.onclick = async function () {
         // event.preventDefault()
         const response = await fetch("/tasks", {
@@ -70,74 +69,76 @@ const renderTasks = function() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({_id: tasks[i]._id})
-        })
+          body: JSON.stringify({ _id: tasks[i]._id }),
+        });
 
         // console.log( response.json() )
-        await updateTasks()
-      }
-      row.appendChild(deleteButton)
+        await updateTasks();
+      };
+      row.appendChild(deleteButton);
     }
 
-    table.appendChild(row)
+    table.appendChild(row);
   }
 
-  const row = document.createElement("tr")
-  const today = (new Date()).toISOString().split('T')[0]
+  const row = document.createElement("tr");
+  const today = new Date().toISOString().split("T")[0];
 
-  row.innerHTML = `<td></td><td><input name="taskName" type="text" form="addForm" placeholder="Task Name" required></td><td><input name="priority" type="number" form="addForm" placeholder="Priority" value="1" required></td><td><input name="creation_date" type="date" form="addForm" value="${today}" required></td><td></td><td><input id="add" type="submit" form="addForm" value="Add Task"/></td>`
-  table.appendChild(row)
+  row.innerHTML = `<td></td><td><input name="taskName" type="text" form="addForm" placeholder="Task Name" required></td><td><input name="priority" type="number" form="addForm" placeholder="Priority" value="1" required></td><td><input name="creation_date" type="date" form="addForm" value="${today}" required></td><td></td><td><input id="add" type="submit" form="addForm" value="Add Task"/></td>`;
+  table.appendChild(row);
 
-  const checkboxes = document.querySelectorAll('.trollCheckbox');
-  checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('click', function(event) {
+  const checkboxes = document.querySelectorAll(".trollCheckbox");
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("click", function (event) {
       event.preventDefault();
-      alert("YOU MAY NOT COMPLETE THE TASK! This is a To Not-Do List, remember?");
+      alert(
+        "YOU MAY NOT COMPLETE THE TASK! This is a To Not-Do List, remember?",
+      );
     });
   });
-}
+};
 
 const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
+  event.preventDefault();
 
-  const form = document.querySelector( "#addForm" )
-  const formData = new FormData(form)
-  const json = {}
-  formData.forEach(function(value, key){
+  const form = document.querySelector("#addForm");
+  const formData = new FormData(form);
+  const json = {};
+  formData.forEach(function (value, key) {
     json[key] = value;
   });
-  const body = JSON.stringify( json )
+  const body = JSON.stringify(json);
 
-  const response = await fetch( "/tasks", {
-    method:"POST",
+  const response = await fetch("/tasks", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: body,
-  })
+  });
 
   // console.log( response.json() )
-  await updateTasks()
-}
+  await updateTasks();
+};
 
-window.onload = async function() {
-  const form = document.querySelector("#addForm")
-  form.onsubmit = submit
+window.onload = async function () {
+  const form = document.querySelector("#addForm");
+  form.onsubmit = submit;
 
-  await updateTasks()
-}
+  await updateTasks();
+};
 
 const updateTasks = async function () {
-  const response = await fetch("/tasks")
-  tasks = await response.json()
+  const response = await fetch("/tasks");
+  tasks = await response.json();
 
   if (tasks.newUserCreated) {
-    alert("New user created!")
+    alert("New user created!");
     // delete tasks.newUserCreated
   }
-  renderTasks()
-}
+  renderTasks();
+};
--- server.improved.js
+++ server.improved.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const GitHubStrategy = require('passport-github').Strategy;
-const passport = require('passport');
-const session = require('express-session');
-const express    = require('express'),
-  app        = express(),
-  { MongoClient, ObjectId } = require("mongodb")
+require("dotenv").config();
+const GitHubStrategy = require("passport-github").Strategy;
+const passport = require("passport");
+const session = require("express-session");
+const express = require("express"),
+  app = express(),
+  { MongoClient, ObjectId } = require("mongodb");
 
 // const appdata = [
 //   {
@@ -29,7 +29,7 @@ const express    = require('express'),
 //   task.days_not_done = Math.floor((new Date() - new Date(task.creation_date)) / (1000 * 60 * 60 * 24))
 // })
 
-app.use( express.json() )
+app.use(express.json());
 
 app.use(
   session({
@@ -41,7 +41,7 @@ app.use(
       secure: false,
       maxAge: 24 * 60 * 60 * 1000,
     },
-  })
+  }),
 );
 
 app.use(passport.initialize());
@@ -60,132 +60,140 @@ passport.use(
     {
       clientID: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      callbackURL: 'https://a3-gabrielshiu.onrender.com/auth/github/callback',
+      callbackURL: "http://localhost:3000/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
       cb(null, profile);
-    }
-  )
+    },
+  ),
 );
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.PASS}@${process.env.HOST}`
-const client = new MongoClient( uri )
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.PASS}@${process.env.HOST}`;
+const client = new MongoClient(uri);
 
-let taskCollection = null
-let userCollection = null
+let taskCollection = null;
+let userCollection = null;
 
 const isAuth = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 };
 
-app.get('/', isAuth, async (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", isAuth, async (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   if (req.user) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
-  res.sendFile(__dirname + '/public/login.html');
+  res.sendFile(__dirname + "/public/login.html");
 });
 
-app.get('/logout', function(req, res, next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
 //auth
-app.get('/auth/github', passport.authenticate('github'));
+app.get("/auth/github", passport.authenticate("github"));
 
 app.get(
-  '/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
   async function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
-  }
+    res.redirect("/");
+  },
 );
 
 async function run() {
-  await client.connect()
-  taskCollection = await client.db("datatest").collection("Tasks")
-  userCollection = await client.db("datatest").collection("Users")
+  await client.connect();
+  taskCollection = await client.db("datatest").collection("Tasks");
+  userCollection = await client.db("datatest").collection("Users");
 
   app.get("/tasks", isAuth, async (req, res) => {
     if (taskCollection !== null) {
-      const user = await userCollection.find({ id: req.user }).toArray()
+      const user = await userCollection.find({ id: req.user }).toArray();
 
       if (!user.length) {
-        await userCollection.insertOne({ id: req.user })
-        return res.json( { newUserCreated: true } )
+        await userCollection.insertOne({ id: req.user });
+        return res.json({ newUserCreated: true });
       }
 
-      const tasks = await taskCollection.find({ userId: req.user }).sort({priority: -1}).toArray()
-      return res.json( tasks )
+      const tasks = await taskCollection
+        .find({ userId: req.user })
+        .sort({ priority: -1 })
+        .toArray();
+      return res.json(tasks);
     }
-  })
+  });
 }
 
-run()
+run();
 
-app.use( (req,res,next) => {
-  if( taskCollection !== null ) {
-    next()
-  }else{
-    res.status( 503 ).send()
+app.use((req, res, next) => {
+  if (taskCollection !== null) {
+    next();
+  } else {
+    res.status(503).send();
   }
-})
+});
 
-app.post('/tasks', isAuth, async function(req, res) {
-  const task = req.body
-  task.priority = Number(task.priority)
-  task.days_not_done = Math.floor((new Date() - new Date(task.creation_date)) / (1000 * 60 * 60 * 24))
+app.post("/tasks", isAuth, async function (req, res) {
+  const task = req.body;
+  task.priority = Number(task.priority);
+  task.days_not_done = Math.floor(
+    (new Date() - new Date(task.creation_date)) / (1000 * 60 * 60 * 24),
+  );
   task.userId = req.user;
 
-  const result = await taskCollection.insertOne( task )
-  res.json( result )
-})
+  const result = await taskCollection.insertOne(task);
+  res.json(result);
+});
 
-app.put('/tasks', isAuth, async function(req, res) {
-  const json = req.body
+app.put("/tasks", isAuth, async function (req, res) {
+  const json = req.body;
   const task = {
     taskName: json.taskName,
     // turn priority into a number
     priority: Number(json.priority),
     creation_date: json.creation_date,
-    days_not_done: Math.floor((new Date() - new Date(json.creation_date)) / (1000 * 60 * 60 * 24)),
-  }
+    days_not_done: Math.floor(
+      (new Date() - new Date(json.creation_date)) / (1000 * 60 * 60 * 24),
+    ),
+  };
 
   const result = await taskCollection.updateOne(
-    { _id: new ObjectId( json._id ) },
+    { _id: new ObjectId(json._id) },
     {
       $set: {
         taskName: task.taskName,
         priority: task.priority,
         creation_date: task.creation_date,
         days_not_done: task.days_not_done,
-      }
-    }
-  )
+      },
+    },
+  );
 
-  res.json( result )
-})
+  res.json(result);
+});
 
-app.delete('/tasks', isAuth, async function(req, res) {
+app.delete("/tasks", isAuth, async function (req, res) {
   const result = await taskCollection.deleteOne({
-    _id:new ObjectId( req.body._id )
-  })
+    _id: new ObjectId(req.body._id),
+  });
 
-  res.json( result )
-})
+  res.json(result);
+});
 
-app.use( express.static( 'public' ) )
+app.use(express.static("public"));
 
-
-app.listen( process.env.PORT || 3000 )
+app.listen(process.env.PORT || 3000);
