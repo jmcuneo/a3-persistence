b8baf09ffb2406e76c282c31469b09848bed2444
Ronak Wani <ronak_wani@outlook.com>
2024-04-02 20:47:44 -0400

    bcrypt and JWT

--- recipes_backend/controllers/recipeController.js
+++ recipes_backend/controllers/recipeController.js
@@ -4,7 +4,7 @@ const Recipe = require("../models/recipeModel");
 //@ route GET /recipes
 //@access public
 const getRecipes = asyncHandler(async (req,res)=>{
-    const recipes = Recipe.find();
+    const recipes = await Recipe.find();
     res.status(200).json(recipes);
 });
 
@@ -18,28 +18,52 @@ const createRecipe = asyncHandler(async (req,res)=>{
         res.status(400);
         throw new Error("All fields are mandatory");
     }
-    res.status(200).json({message: "Create recipe"});
+    const recipe = await Recipe.create({
+        recipe_name, recipe_ingredients, recipe_description
+    });
+    res.status(200).json(recipe);
 });
 
 //@desc Get particular recipe
 //@ route GET /recipes/:id
 //@access public
 const getRecipe = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Get recipe for ${req.params.id}`});
+    const recipe = await Recipe.findById(req.params.id);
+    if(!recipe){
+        res.status(404);
+        throw new Error("Recipe Not Found");
+    }
+    res.status(200).json(recipe);
 });
 
 //@desc Update a recipe
 //@ route PUT /recipes/:id
 //@access public
 const updateRecipe = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Update recipe for ${req.params.id}`});
+    const recipe = await Recipe.findById(req.params.id);
+    if(!recipe){
+        res.status(404);
+        throw new Error("Recipe Not Found");
+    }
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+    res.status(200).json(updatedRecipe);
 });
 
 //@desc Delete a recipe
 //@ route DELETE /recipes/:id
 //@access public
 const deleteRecipe = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Delete recipe for ${req.params.id}`});
+    const recipe = await Recipe.findById(req.params.id);
+    if(!recipe){
+        res.status(404);
+        throw new Error("Recipe Not Found");
+    }
+    await Recipe.remove();
+    res.status(200).json(recipe);
 });
 
 module.exports = {getRecipes, createRecipe, getRecipe, updateRecipe, deleteRecipe};
--- recipes_backend/server.js
+++ recipes_backend/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler")
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbConnection")
-connectDb().then((connection) => {
-    const app = express();
-    const port = process.env.PORT;
-    app.use(express.json());
-    app.use("/recipes", require("./routes/recipesRoutes"));
-    app.use(errorHandler);
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
-})
+connectDb();
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+app.use("/recipes", require("./routes/recipesRoutes"));
+app.use("/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
