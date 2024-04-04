ceced95a297c92d43caa1a632a72c4f2543b4861
jsilvester13 <jacobsilvester0308@gmail.com>
2024-04-03 22:44:19 -0400

    working database, beginning login

--- public/js/main.js
+++ public/js/main.js
@@ -1,87 +1,102 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
+//THIS SHOULD WORK
+const submit = async function (event) {
+  event.preventDefault()
 
-const app = express();
-const port = 3000;
-const mongoUrl = "mongodb://localhost:27017"; // Your MongoDB connection URL
-const dbName = "JacobsA3Database"; // Your MongoDB database name
-const collectionName = "A3Dataset"; // Your MongoDB collection name
+  const frontString = document.querySelector("#frontstring").value, //getting string values from text input fields
+    backString = document.querySelector("#backstring").value,
+    concatenatedString = frontString + ' ' + backString, //putting the inputs together into one string variable
+    stringLength = concatenatedString.length;
+  let body = JSON.stringify({content: concatenatedString}) //creating body, which will contain the desired method and required data, in this case submit and the string
+  const response = await fetch("/submit", { //POST the body
+    method: "POST",
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  updateTable(); //update the table after submission
+}
 
-app.use(bodyParser.json());
+async function deleteEntry(id){
+  const method = "/delete";
+  let body = JSON.stringify({content:id});
+  const response = await fetch("/delete", {
+    method: "POST",
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  updateTable();
+}
 
-// Connect to MongoDB
-let db;
+async function editEntry(id, entry){
+  const method = "/edit";
+  let body = JSON.stringify({id:id, content:entry });
+  const response = await fetch("/edit", {
+    method: "POST",
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  updateTable();
+}
+//updateTable function
+async function updateTable() {
+  //get the current state of the array from the server
+  const response = await fetch("/docs")
+  let currentDatabase = await response.json();
+  const tableElement = document.getElementById('responseTable'); //located the table element in the HTML
+  tableElement.innerHTML = ''; // Clear existing table contents
 
-MongoClient.connect(mongoUrl, { useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.error("Error connecting to MongoDB:", err);
-    return;
-  }
-  console.log("Connected to MongoDB successfully");
-  db = client.db(dbName);
-});
+  tableElement.innerHTML = ''; // Clear existing table contents
 
-// Route for GET requests
-app.get("/getArray", async (req, res) => {
-  try {
-    const result = await db.collection(collectionName).find().toArray();
-    res.json(result);
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    res.status(503).send("Internal server error");
-  }
-});
+  // Create table headers
+  const headerRow = document.createElement('tr');
+  const headers = ['String ID', 'CombinedString', 'String Length', 'Edit', 'Delete']; // Adjust these according to your data fields
+  headers.forEach(headerText => {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = headerText;
+    headerRow.appendChild(headerCell);
+  });
+  tableElement.appendChild(headerRow);
 
-// Route for POST requests
-app.post("/submit", async (req, res) => {
-  const combinedString = req.body.string;
-  try {
-    await db.collection(collectionName).insertOne({ combinedString });
-    res.send("Submitted!");
-  } catch (err) {
-    console.error("Error adding data:", err);
-    res.status(503).send("Internal server error");
-  }
-});
+  // Populate table rows with data from MongoDB collection
+  currentDatabase.forEach((entry, index) => {
+    const row = tableElement.insertRow(-1);
+    // Iterate over each field in the entry
+    Object.keys(entry).forEach((key, idx) => {
+      const cell = row.insertCell();
+      cell.textContent = entry[key];
+    });
 
-app.post("/add", async (req, res) => {
-  const combinedString = req.body.string;
-  try {
-    await db.collection(collectionName).insertOne({ combinedString });
-    res.send("Added!");
-  } catch (err) {
-    console.error("Error adding data:", err);
-    res.status(503).send("Internal server error");
-  }
-});
+    // Add a cell for the edit button
+    const editCell = row.insertCell();
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', function () {
+      const newInput = prompt('Enter new value: ');
+      editEntry(entry._id, newInput);
+    });
+    editCell.appendChild(editButton);
 
-app.post("/delete", async (req, res) => {
-  const index = req.body.index;
-  try {
-    await db.collection(collectionName).deleteOne({ _id: ObjectId(index) });
-    res.send("Deleted!");
-  } catch (err) {
-    console.error("Error deleting data:", err);
-    res.status(503).send("Internal server error");
-  }
-});
+    // Add a cell for the delete button
+    const deleteCell = row.insertCell();
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+      deleteEntry(entry._id)
+    });
+    deleteCell.appendChild(deleteButton);
+  });
+  
+}
 
-app.post("/edit", async (req, res) => {
-  const index = req.body.index;
-  const content = req.body.content;
-  try {
-    await db.collection(collectionName).updateOne(
-      { _id: ObjectId(index) },
-      { $set: { combinedString: content } }
-    );
-    res.send("Edited!");
-  } catch (err) {
-    console.error("Error editing data:", err);
-    res.status(503).send("Internal server error");
-  }
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+window.onload = function () {
+  updateTable();
+  const button = document.querySelector("button");
+  button.onclick = submit;
+}
--- server.improved.js
+++ server.improved.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookie  = require( "cookie-session" );
 const fs = require("fs");
 const mime = require("mime");
 const dir = "public/";
@@ -6,10 +7,19 @@ const port = 3000;
 
 const appdata = [];
 const app = express();
-app.use( express.static( 'public' ) );
-app.use( express.static( 'views'  ) ); 
-app.use(express.static("public") );
-app.use(express.json() );
+app.use( express.urlencoded({ extended:true }) )
+
+// cookie middleware! The keys are used for encryption and should be
+// changed
+app.use( cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
@@ -22,6 +32,53 @@ const client = new MongoClient(uri, {
 });
 
 let collection = null;
+
+//-----------------------------------------------------------------------------
+
+
+
+app.post( '/login', (req,res)=> {
+  debugger
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  console.log( req.body )
+  
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  if( req.body.password === 'test' ) {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+    
+    // since login was successful, send the user to the main content
+    // use redirect to avoid authentication problems when refreshing
+    // the page or using the back button, for details see:
+    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    res.redirect( 'index.html' )
+  }else{
+    // cancel session login in case it was previously set to true
+    req.session.login = false
+    // password incorrect, send back to login page
+    res.render('login', { msg:'login failed, please try again', layout:false })
+  }
+})
+
+
+
+// add some middleware that always sends unauthenicaetd users to the login page
+app.use( function( req,res,next) {
+  if( req.session.login === true )
+    next()
+  else
+    res.render('login', { msg:'login failed, please try again', layout:false })
+})
+
+app.get( '/main.html', ( req, res) => {
+    res.render( 'index', { msg:'success you have logged in', layout:false })
+})
+
+//-----------------------------------------------------------------------------
 
 async function run() {
   await client.connect()
@@ -39,9 +96,17 @@ async function run() {
 
 run();
 
+app.get("/docs", async (req, res) => {
+    if (collection !== null) {
+      const docs = await collection.find({}).toArray()
+      res.json( docs )
+    }
+  })
 
-app.get("/getArray", (req, res) => {
-  res.json(appdata);
+
+
+app.get("/", (req, res) => {
+  sendFile(res, 'public/login.html');
 });
 
 app.post("/", (req, res) => {
@@ -52,7 +117,7 @@ app.post("/", (req, res) => {
     const targetIndex = finalData.index;
     appdata.splice(targetIndex, 1);
     res.send("Bye bye!");
-  } else if (method === "/add" || method === "/submit") {
+  } else if (method === "/add") {
     appdata.push(finalData.string);
     res.send("Added/Submitted!");
   } else if (method === "/edit") {
@@ -74,10 +139,61 @@ app.use( (req,res,next) => {
 })
 
 app.post( '/submit', async (req,res) => {
-  const result = await collection.insertOne( req.body )
+  const myCollection = await client.db("JacobsA3Database").collection("A3Dataset");
+  const combString = req.body.content;
+  const strL = req.body.content.length;
+  //console.log(strL)
+  const result = await myCollection.insertOne( {CombinedString : combString,
+                                                StringLength : strL} );
+  
+  res.json( result );
+  //console.log("Combined string is: " + JSON.stringify(req.body))
+  //console.log(req.body)
+})
+
+// assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
+app.post( '/delete', async (req,res) => {
+  const myCollection = await client.db("JacobsA3Database").collection("A3Dataset");
+  //console.log(req.body.content)
+  const result = await myCollection.deleteOne({ 
+    _id:new ObjectId( req.body.content ) 
+  })
+  
   res.json( result )
 })
 
+app.post( '/edit', async (req,res) => {
+  const myCollection = await client.db("JacobsA3Database").collection("A3Dataset");
+  const combString = req.body.content;
+  const strL = req.body.content.length;
+  const result = await myCollection.updateOne(
+    { _id: new ObjectId( req.body.id ) },
+    { $set:{ CombinedString:req.body.content,
+             StringLength : strL}}
+  )
+
+  res.json( result )
+})
+
+app.use( express.static( 'public' ) );
+app.use( express.static( 'views'  ) ); 
+app.use(express.static("public") );
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+   fs.readFile( filename, function( err, content ) {
+     // if the error = null, then we"ve loaded the file successfully
+     if( err === null ) {
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { "Content-Type": type })
+       response.end( content )
+     }else{
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( "404 Error: File Not Found" )
+     }
+   })
+}
 
 
 app.listen(port)
@@ -85,4 +201,8 @@ app.listen(port)
 
 /*app.get("/", (req, res) => {
   sendFile(res, "public/index.html");
+});*/
+
+/*app.get("/getArray", (req, res) => {
+  res.json(collection);
 });*/
