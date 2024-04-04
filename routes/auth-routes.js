8353373e78c42044abeeb52aaf37f3436c936ee7
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2024-04-04 14:21:50 -0400

    routes for passport

---
+++ routes/auth-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router(); //create instnce of route
+const passport = require('passport');
+//auth login
+router.get('/login', (req, res) =>{
+    res.render('login')
+})
+
+router.get('/github', passport.authenticate('github', {
+    scope: ['profile']
+}));
+
+
+//callback route for github -> tell passport to give us the profile info from the code
+router.get('/github/redirect', passport.authenticate('github'), (req, res) => {
+    res.send(req.user);
+})
+
+/*(req, res) => {
+    //passport
+    res.send('logging in with github');
+    console.log("github");
+});
+*/
+
+module.exports = router;
