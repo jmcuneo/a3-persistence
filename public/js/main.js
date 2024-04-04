de701cf5c4a1509670f9e54552ddc3cd8be30d4a
Nat <dynkon@gmail.com>
2024-04-03 20:17:05 -0400

    Bulk of the work haha

--- public/js/main.js
+++ public/js/main.js
@@ -1,83 +1,161 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 // credit to: https://www.valentinog.com/blog/html-table/
 let returnedArray;
+let currID;
 
-function generateTableHead(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
-}
 
-function generateTable(table, data) {
-  for (let element of data) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
-  }
-}
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
-  const input_model = document.querySelector( "#model" ),
-        input_year = document.querySelector( "#year" ),
-        input_mpg = document.querySelector( "#mpg" ),
-        json = { model: input_model.value,
-                year: Number(input_year.value),
-                mpg: Number(input_mpg.value) },
-        body = JSON.stringify( json )
+  const input_model = document.querySelector("#model"),
+    input_year = document.querySelector("#year"),
+    input_mpg = document.querySelector("#mpg"),
+    json = {
+      model: input_model.value,
+      year: Number(input_year.value),
+      mpg: Number(input_mpg.value)
+    },
+    body = JSON.stringify(json)
 
-  const response = await fetch( "/submit", {
-    method:"POST",
-    headers: {'Content-Type': 'application/json'},
-    body 
+  const response = await fetch("/add", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body
   })
+  alert("Successfully submitted!")
+
+}
+
+const erase = async function () {
+
+  const response = await fetch("/remove", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({_id: currID})
+  })
+  const text = await response.text()
+  let returnedArray = JSON.parse(text);
+  console.log("text:", returnedArray);
+
+  alert("Successfully deleted! Please refresh to see changes")
+}
+
+const view = async function () {
+  document.getElementById("view").style.display = "none"
+  const response = await fetch("/docs", { method: "GET" })
 
   const text = await response.text()
+
+  let returnedArray = JSON.parse(text);
+  console.log("text:", returnedArray);
+  createTable(returnedArray);
+}
+
+const update = async function () {
+
+  const input_model = document.querySelector("#model"),
+    input_year = document.querySelector("#year"),
+    input_mpg = document.querySelector("#mpg"),
+    json = {
+      _id: currID,
+      model: input_model.value,
+      year: Number(input_year.value),
+      mpg: Number(input_mpg.value)
+    },
+    body = JSON.stringify(json)
+
+  const response = await fetch("/update", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body
+  })
+
+  alert("Successfully updated! Please refresh to see changes")
+
+}
+
+
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  let test = ""
+ 
+  for (let key of data) {
+      let th = document.createElement("th");
+      if (key !== "_id"){
+        test = key
+      }
+      let text = document.createTextNode(test);
+     
+      th.appendChild(text);
+      row.appendChild(th);
+    
+  }
+}
+
+function generateTable(data) {
+  let table = document.createElement("table")
+  let insertHere = document.getElementById("insertTable")
   
-  let returnedArray = JSON.parse(text);
-  console.log( "text:", returnedArray );
-  createTable(returnedArray);
+  table.className = "lobster-regular"
+
+  for (let element of data) {
+    let row = table.insertRow();
+    let currRoundID;
+    for (key in element) {
+      let cell = row.insertCell();
+      if (key === "_id") {
+        let del = document.createElement("button")
+        currRoundID = element[key]
+        del.id = currRoundID
+        del.class = "delete"
+        del.onclick = function(){
+          currID = del.id
+          erase()
+        }
+        del.appendChild(document.createTextNode("delete"))
+        cell.appendChild(del)
+      } else {
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+    cell = row.insertCell()
+    let upd = document.createElement("button")
+    upd.id = currRoundID
+    upd.class = "update"
+    upd.onclick = function () {
+      currID = upd.id
+      console.log(currID)
+      update()
+    }
+    upd.appendChild(document.createTextNode("update"))
+    cell.appendChild(upd)
+  }
+  insertHere.appendChild(table)
 }
 
-const erase = async function( event ) {
-  event.preventDefault()
-
-  const valueToDel = { "number" : document.getElementById("row").value };
-
-  const response = await fetch( "/submit", {
-    method:"DELETE",
-    headers: {'Content-Type': 'application/json'}, 
-    body: JSON.stringify(valueToDel) 
-  })
-  const text = await response.text()
-  let returnedArray = JSON.parse(text);
-  console.log( "text:", returnedArray );
-  createTable(returnedArray);
-}
-
-const createTable = function(array) {
+const createTable = function (array) {
+  generateTable(array);
+  let data = Object.keys(array[0]);
   let table = document.querySelector("table");
-  let data = Object.keys(array [0]);
-  generateTable(table, array);
   generateTableHead(table, data);
 }
 
-window.onload = function() {
-   const button_submit = document.getElementById("submit");
-   const button_delete = document.getElementById("erase");
-  button_submit.onclick = submit;
-  button_delete.onclick = erase;
+
+window.onload = function () {
+  const button_submit = document.getElementById("submit");
+  const button_view = document.getElementById("view")
   
+  button_submit.onclick = submit;
+  button_view.onclick = view;
+  
+  
+
+
 }
--- server.js
+++ server.js
@@ -1,39 +1,108 @@
-const express = require( 'express' ),
-      app = express()
+const express = require('express'),
+  app = express()
+
+require('dotenv').config()
 const appdata = [
-        { "model": "toyota", "year": 1999, "mpg": 23, "EOL": 2077},
-        { "model": "honda", "year": 2004, "mpg": 30, "EOL": 2060},
-        { "model": "ford", "year": 1987, "mpg": 14, "EOL": 2049} 
-      ]
+  { "model": "toyota", "year": 1999, "mpg": 23, "EOL": 2077 },
+  { "model": "honda", "year": 2004, "mpg": 30, "EOL": 2060 },
+  { "model": "ford", "year": 1987, "mpg": 14, "EOL": 2049 }
+]
 
 app.use(express.static('public'))
 app.use(express.json())
 
+//DATABASE CONNECTION START
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
 
-const calculateEOL = (year, mpg) => {
-    let new_val = year + mpg;
-    new_val = new_val - (year % mpg);
-  
-    return new_val;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
+});
 
-  app.post( '/submit', (req, res) => {
-    req.body.EOL = calculateEOL(req.body.year, req.body.mpg)
-    appdata.push(req.body)
-    res.writeHead( 200, { 'Content-Type': 'application/json' })
-    res.end( JSON.stringify( appdata ) )
-  })
+let collection = null
 
-  app.delete( '/submit', (req, res) => {
-    let index = Number(req.body.number)
-    if (index > -1){
-        appdata.splice(index, 1)
-        res.writeHead( 200, { 'Content-Type': 'application/json' })
-        res.end( JSON.stringify( appdata ) )
-    } else {
-        res.writeHead( 410, { 'Content-Type': 'text/plain' })
-        res.end("There was nothing to delete!")
+async function run() {
+
+
+  await client.connect();
+
+  collection = await client.db("Cars").collection("myCars");
+
+  app.get("/docs", async (req, res) => {
+    if (collection !== null) {
+      const docs = await collection.find({}).toArray()
+      res.json(docs)
     }
   })
+}
+run()
 
-  app.listen(3000)
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next()
+  } else {
+    res.status(503).send()
+  }
+})
+
+app.post('/add', async (req, res) => {
+  let temp = req.body
+  temp.EOL = calculateEOL(temp.year, temp.mpg)
+  const result = await collection.insertOne(temp)
+  res.json(result)
+})
+
+app.post('/remove', async (req, res) => {
+  const result = await collection.deleteOne({
+    _id: new ObjectId(req.body._id)
+  })
+
+  res.json(result)
+})
+
+app.post('/update', async (req, res) => {
+  const result = await collection.updateOne(
+    { _id: new ObjectId(req.body._id) },
+    { $set: { model: req.body.model,
+      year: req.body.year, 
+      mpg: req.body.mpg,
+      EOL: calculateEOL(req.body.year, req.body.mpg)} }
+  )
+
+  res.json(result)
+})
+//DATABASE CONNECTION END
+
+
+const calculateEOL = (year, mpg) => {
+  let new_val = year + mpg;
+  new_val = new_val - (year % mpg);
+
+  return new_val;
+}
+
+app.post('/submit', (req, res) => {
+  req.body.EOL = calculateEOL(req.body.year, req.body.mpg)
+  appdata.push(req.body)
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(appdata))
+})
+
+app.delete('/submit', (req, res) => {
+  let index = Number(req.body.number)
+  if (index > -1) {
+    appdata.splice(index, 1)
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(appdata))
+  } else {
+    res.writeHead(410, { 'Content-Type': 'text/plain' })
+    res.end("There was nothing to delete!")
+  }
+})
+
+app.listen(3000)
