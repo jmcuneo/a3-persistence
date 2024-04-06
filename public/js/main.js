13a5ddeeeb74ce4772bbf866f975541f94424209
sfanning5 <stephen.e.fanning@gmail.com>
2024-04-06 00:48:07 -0400

    Completed basic functionality

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,39 @@ const populateHeaders = function()
   })
 }
 
+const populateTable = async function(response)
+{
+  response = await response.json()
+  console.log(response)
+  table = document.getElementById("dataBody")
+  table.innerHTML = ""
+  populateHeaders()
+  response.forEach(rowData => {
+    let row = document.createElement("tr")
+
+    rowData.forEach(cellData => {
+      let cell = document.createElement("td")
+      cell.textContent = cellData
+      row.appendChild(cell)
+    })
+    table.appendChild(row)
+  })
+}
+
+const clear = async function (event) {
+  console.log("CLEARING")
+  if(event)
+  {
+    event.preventDefault()
+  }
+
+  await fetch("/clear", {
+    method: "POST"
+  }).then(response => {
+    populateTable(response)
+  })
+}
+
 const submit = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
@@ -23,34 +56,30 @@ const submit = async function( event ) {
   const course = document.querySelector( "#class" ),
         task = document.querySelector("#task"),
         time = document.querySelector("#time"),
-        json = {data: [course.value, task.value, time.value]
+        includeTime = document.getElementById("includeTime")
+        json = {data: [course.value, task.value, time.value, includeTime.checked]
                },
         body = JSON.stringify(json)
 
   await fetch( "/submit", {
     method:"POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body 
   })
-  .then(response => response.json())
   .then(response => {
-    table = document.getElementById("dataBody")
-    table.innerHTML = ""
-    populateHeaders()
-    response.forEach(rowData => {
-      let row = document.createElement("tr")
-
-      rowData.forEach(cellData => {
-        let cell = document.createElement("td")
-        cell.textContent = cellData
-        row.appendChild(cell)
-      })
-      table.appendChild(row)
-    })
+    populateTable(response)
   })
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
-  populateHeaders()
+window.onload = function () {
+  const submitButton = document.getElementById("submitButton");
+  submitButton.onclick = submit;
+  const clearButton = document.getElementById("clearButton");
+  clearButton.onclick = clear;
+  
+  fetch("/update", {
+    method: "POST"
+  }).then(response => populateTable(response))
 }
