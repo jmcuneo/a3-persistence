a41fdb5c22aced58e25ae6985e5fe7241e9cf8e3
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2024-04-06 00:53:30 -0400

    deleted unused functions

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,9 @@
-const nextPage = function(event){
-    event.preventDefault();
-    window.location.href = "/index.html"
-}
 const nextPageGithub = function(event){
     event.preventDefault();
     window.location.href = "/auth/github"
 }
-
 window.onload = function () {
     console.log("hello world");
-    //document.getElementById("login").onclick = nextPage;
     document.getElementById("logingithub").onclick = nextPageGithub;
 
 }
